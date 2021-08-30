00dffac4d12121dea8af811c21e09635c0f1df62
Yasniel Laptop <yasnielfajardoegues1408@gmail.com>
2021-08-30 15:05:00 -0700

    add eslint, prettierrc and husky

--- apps/user-ui/pages/index.js
+++ apps/user-ui/pages/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import Button from "@monorepo-next-example/button";
+import React from 'react';
+import Button from '@monorepo-next-example/button';
+
 const Index = () => {
-  return (
-    <div>
-      <Button isSelected onClick={() => alert("Hello!")}>
-        Hello World!
-      </Button>
-    </div>
-  );
+    return (
+        <div>
+            <Button isSelected onClick={() => alert('Hello!')}>
+                Hello World!
+            </Button>
+        </div>
+    );
 };
 export default Index;
