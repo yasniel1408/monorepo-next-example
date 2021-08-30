fdd570be0d5f04c7bf006910d1f5b419eed58af6
Yasniel Laptop <yasnielfajardoegues1408@gmail.com>
2021-08-30 14:36:29 -0700

    first commit

---
+++ apps/user-ui/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@monorepo-next-example/button";
+const Index = () => {
+  return (
+    <div>
+      <Button isSelected onClick={() => alert("Hello!")}>
+        Hello World!
+      </Button>
+    </div>
+  );
+};
+export default Index;
